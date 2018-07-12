690b1f83f5f368a1eee2c93e0025526804e992b6
acuicultor <ramoncuenca4@gmail.com>
2018-07-12 16:21:43 +0200

    Build script: use stock toolchain from Google

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ INCREMENTAL_VERSION="V1.0.0"
 export LOCALVERSION="${KERNEL_NAME}-${INCREMENTAL_VERSION}"
 export KBUILD_BUILD_USER=acuicultor
 export KBUILD_BUILD_HOST=NuclearPowerPlant
-export PATH=$PATH:/home/acuicultor/Android/toolchains/ubernaro-aarch64-linux-android-4.9/bin
+export PATH=$PATH:/home/acuicultor/Android/toolchains/aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
 make ARCH=arm64 distclean
 rm -rf out/
