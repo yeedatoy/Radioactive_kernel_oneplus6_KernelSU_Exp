a20632e7204f7ebb498fd4e0b3047da8e340abad
acuicultor <ramoncuenca4@gmail.com>
2018-07-29 21:24:09 +0200

    Update build version
    * Switch to linaro toolchain too

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Radioactive Kernel Details
 KERNEL_NAME="~Radioactive"
-INCREMENTAL_VERSION="V1.0.1"
+INCREMENTAL_VERSION="V1.0.2"
 export LOCALVERSION="${KERNEL_NAME}-${INCREMENTAL_VERSION}"
 export KBUILD_BUILD_USER=acuicultor
 export KBUILD_BUILD_HOST=NuclearPowerPlant
-export PATH=$PATH:/home/acuicultor/Android/toolchains/aarch64-linux-android-4.9/bin
+export PATH=$PATH:/home/acuicultor/Android/toolchains/linaro-aarch64-linux-android-4.9/bin
 export CROSS_COMPILE=aarch64-linux-android-
 make ARCH=arm64 distclean
 rm -rf out/
