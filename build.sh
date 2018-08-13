1a2e272ffa23668f63f458408bd4b8e8b8ac9a3e
acuicultor <ramoncuenca4@gmail.com>
2018-08-13 09:58:58 +0200

    Update build version

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ txtrst=$(tput sgr0)             # reset
 blink=$(tput blink)             # blink
 
 KERNEL_NAME="~Radioactive"
-INCREMENTAL_VERSION="V1.0.7"
+INCREMENTAL_VERSION="V1.0.8"
 export LOCALVERSION="${KERNEL_NAME}-${INCREMENTAL_VERSION}"
 export KBUILD_BUILD_USER=acuicultor
 export KBUILD_BUILD_HOST=NuclearPowerPlant
