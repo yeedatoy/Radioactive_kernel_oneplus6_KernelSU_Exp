255fdcad6c8091ff00a657dd32355b660ef87472
acuicultor <ramoncuenca4@gmail.com>
2019-07-29 16:32:54 +0200

    Revert "UPSTREAM: ANDROID: binder: remove 32-bit binder interface."
    This reverts commit b8c5d33d0ec243706667e111ce22b8d2107507bb.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -73,6 +73,10 @@
 
 #include <linux/proc_fs.h>
 
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
+#define BINDER_IPC_32BIT 1
+#endif
+
 #include <uapi/linux/android/binder.h>
 #include "binder_alloc.h"
 #include "binder_trace.h"
