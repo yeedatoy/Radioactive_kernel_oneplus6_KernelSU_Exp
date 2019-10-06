f47843d9b2789e14757dd91e1a63cf66078c34aa
acuicultor <ramoncuenca4@gmail.com>
2019-10-06 11:29:03 +0200

    Revert "msm: performance: prevent the driver from handling userspace boosts"
    This reverts commit a71932b2704bad57fa164758213f47441f32f13a.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -69,7 +69,6 @@ device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
  */
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
-#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -133,7 +132,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
-#endif
 
 	return 0;
 }
@@ -162,7 +160,6 @@ module_param_cb(cpu_min_freq, &param_ops_cpu_min_freq, NULL, 0644);
  */
 static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 {
-#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -218,7 +215,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
-#endif
 
 	return 0;
 }
