0a986df3e8e14a327186403dccee3021d216d523
acuicultor <ramoncuenca4@gmail.com>
2020-07-19 11:09:28 +0200

    boost: set default values

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1038,8 +1038,8 @@ static void write_default_values(struct cgroup_subsys_state *css)
 {
 	u8 i;
 	char cg_name[11];
-	const int boost_values[4] = { 5, 5, 0, -30 };
-	const bool prefer_idle_values[4] = { 0, 1, 1, 0 };
+	const int boost_values[4] = { 1, 20, 0, 0 };
+	const bool prefer_idle_values[4] = { 0, 0, 0, 0 };
 	const char *stune_groups[] =
 	{ "/", "top-app", "foreground", "background" };
 
