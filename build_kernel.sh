93f60d0d2fc7ce7354a97407ecd75443d7d2cd65
Coconut <coconutat@protonmail.com>
2023-06-07 18:02:02 +0800

    add a simple build script

---
+++ build_kernel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#设置环境
+
+# 交叉编译器路径
+export PATH=$PATH:/home/coconutat/github/proton-clang-master/bin
+export CC=clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+export ARCH=arm64
+export SUBARCH=arm64
+# export DTC_EXT=dtc
+
+if [ ! -d "out" ];
+then
+	mkdir out
+fi
+
+date="$(date +%Y.%m.%d-%I:%M)"
+
+
+make ARCH=arm64 O=out CC=clang radioactive_defconfig
+# 定义编译线程数
+make ARCH=arm64 O=out CC=clang -j24 2>&1 | tee kernel_log-${date}.txt
+
